43ed52e64e1fd58c4172826fb76dd5d7812062f2
Mdforidulislam <mdforidulislam962@gmail.com>
2024-02-06 00:51:26 +0600

    added the new text her

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 
-import logo from "../../../public/logo.jpg";
 import Navbar from "./Navbar";
 import Container from "@/Share/Container";
 
@@ -10,7 +9,8 @@ const Header = () => {
     <Container>
       <div className="flex justify-between h-full items-center shadow-sm p-2">
         <div>
-          <Image src={logo} width={80} height={80} alt="logo" />
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, vero molestias ullam ipsam distinctio exercitationem ipsum voluptatibus cupiditate ratione, ipsa quisquam aliquam doloremque! Vero, inventore, quo minus tenetur quae ipsam ex ipsum velit quas doloribus beatae illum dolorum nisi voluptates iusto corporis dolore tempore expedita. Vitae, quasi iusto! Quaerat nam odit rerum obcaecati, nisi magni mollitia explicabo beatae blanditiis quis! Veniam iure ea sequi omnis excepturi laudantium quidem, quam incidunt est provident perferendis eligendi accusantium possimus reprehenderit et quas consectetur veritatis nostrum obcaecati repellat! Consequatur possimus quisquam deserunt blanditiis magni facere in nobis sapiente. Et asperiores placeat nesciunt repellat dolorum!
+         </p>
         </div>
         <div>
           <Navbar />

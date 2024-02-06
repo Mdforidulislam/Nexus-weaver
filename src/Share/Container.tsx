e39664f2133d131b5c9ec9a2f47fd5b5fe409ea9
saarif23 <arif23s.001@gmail.com>
2024-02-06 11:49:51 +0600

    add header animation

--- src/Share/Container.tsx
+++ src/Share/Container.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
 
-interface ShareProps{
-    children: ReactNode;
+interface ShareProps {
+  children: ReactNode;
 }
 
-const Container: React.FC<ShareProps> = ({children}) =>{
-    return(
-        <div className="max-w-screen-xl mx-auto px-4">
-            {children}
-        </div>
-    )
-}
+const Container: React.FC<ShareProps> = ({ children }) => {
+  return (
+    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+      {children}
+    </div>
+  );
+};
 
 export default Container;

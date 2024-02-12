a4c9eb33e3af3d6fa3ba6f4ebc49747b68c141a3
saarif23 <arif23s.001@gmail.com>
2024-02-12 20:31:14 +0600

    talkidea and about section responsive in small medium and large device

--- src/Components/TalkIdea/TalkIdeas.tsx
+++ src/Components/TalkIdea/TalkIdeas.tsx
@@ -13,23 +13,23 @@ const TalkIdeas = () => {
     },
   };
   return (
-    <div className="lg:mb-20">
+    <div className="my-10">
       <Container>
-        <div className="block md:flex justify-center items-center gap-14">
-          <div className="space-y-3">
-            <h3 className="text-5xl font-bold">
-              Let&apos;s talk about your idea <br /> today
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-14">
+          <div className="flex-1 space-y-3 text-center lg:text-left ">
+            <h3 className="text-4xl lg:text-5xl  font-bold">
+              Let&apos;s talk about your idea today
             </h3>
-            <p className="w-2/4">
+            <p className="">
               Your ideas are the driving factor behind our efforts. Whether you
               have a clear vision or need assistance fleshing out your idea,
-              we&apos;re here to listen and cooperate with you every step of the way.
-              Let us work together to make your vision a reality.
+              we&apos;re here to listen and cooperate with you every step of the
+              way. Let us work together to make your vision a reality.
             </p>
 
             <Button btnText="Let's talk to us" />
           </div>
-          <div>
+          <div className="flex-1">
             <Lottie options={defaultOptions} height={400} width={400}></Lottie>
           </div>
         </div>
